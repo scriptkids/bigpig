288cd7bc66731256968c10fb374879c6db4bc376
scriptkids <zjy3186511@163.com>
2013-04-18 23:31:08 +0800

    增加debug开关\n添加notice函数\n增加access_

--- log.c
+++ log.c
@@ -4,10 +4,37 @@ void err_log(char *buf)
     write(STDERR_FILENO, buf, strlen(buf));
 }
 
+#if DEBUG 
 void debug(const char* format, ...)
 {
     va_list args;
     va_start(args, format);
+    fprintf(stderr, "DEBUG:");
     vfprintf(stderr, format, args);
+    va_end(args);
+}
+#else
+void debug(const char* format, ...)
+{
+    ;
+}
+#endif
+
+void notice(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    fprintf(stderr, "NOTICE:");
+    vfprintf(stderr, format, args);
+    va_end(args);
+}
+
+void access_log(FILE* fp, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+//    vfprintf(stderr, format, args);
+    vfprintf(fp, format, args);
+    fflush(fp);
     va_end(args);
 }
--- log.h
+++ log.h
@@ -2,4 +2,6 @@
 #define __LOG__
 void err_log(char *buf);
 void debug(const char* format, ...);
+void access_log(FILE* fp, const char* format, ...);
+void notice(const char* format, ...);
 #endif
