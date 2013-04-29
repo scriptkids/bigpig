cf7a44e3d0b0b0b49adabaded27912546db403ff
scriptkids <zjy3186511@163.com>
2013-04-29 16:27:56 +0800

    新文件：    server_epoll2.c

---
+++ server_epoll2.c
@@ -0,0 +1,135 @@
+/*try to use epoll 
+ * epfd = epoll_create();
+ * ev.events = 
+ * ev.data.fd = 
+ * epoll_ctl();
+ * epoll_wait();
+ * 检查epoll_wait的返回值，并for循环进行判断。当fd = listenfd时accept。
+ * 不等于listenfd时，进行doit操作，对events[n].data.fd
+ */
+#include "bp.h"
+
+#define MAX_EVENTS 1000
+
+int main(void)
+{
+    struct sockaddr_in servaddr, cliaddr;
+    int listenfd, epfd,connfd,n,nfds;
+    socklen_t clilen;
+    struct epoll_event ev,events[MAX_EVENTS];
+    char buf[1024];
+    bzero(buf,sizeof(buf));
+    
+    server_fp   =   fopen(SERVER_LOG, "a+");
+    access_fp   =   fopen(ACCESS_LOG, "a+");
+    if(NULL == server_fp) {
+        NOTICE("open SERVER_LOG error!!");
+        exit(1);
+    } 
+    if(NULL == access_fp) {
+        NOTICE("open ACCESS_LOG error!!");
+        exit(1);
+    }
+
+    listenfd = tcp_listen(&servaddr);
+    /*need fix*/
+    access_log(server_fp, "server started at prort:%d\n", ntohs(servaddr.sin_port));
+    //NOTICE("server started at PORT %d\n",ntohs(servaddr.sin_port));
+    epfd = epoll_create(20);
+
+    ev.events = EPOLLIN;
+    ev.data.fd = listenfd; 
+    if(epoll_ctl(epfd, EPOLL_CTL_ADD, listenfd, &ev) == -1) {
+        perror("epoll_ctl: listenfd"); 
+    }
+
+
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+    }
+
+    if (pid > 0) { //parents
+        pid_t ppid;
+        ppid = fork();
+        if (ppid < 0) {
+            perror("fork");
+        }
+        if (ppid > 0) {
+            DEBUG("This is the parents");
+        }
+        if (0 == ppid) {
+            while(1) { 
+                DEBUG("begin while");  
+                nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+                //printf("nfds == %d\n",nfds);
+                NOTICE("pid is %d epoll_wait nfds == %d\n",getpid(), nfds);
+                for (n = 0; n < nfds; n++) {
+                    if (events[n].data.fd == listenfd) {
+                        connfd = accept(listenfd, (struct sockaddr *)\
+                                &cliaddr, &clilen);
+                        NOTICE("pid is %d connfd is %d",getpid(), connfd);
+                        ev.events = EPOLLIN | EPOLLET;
+                        ev.data.fd = connfd;
+                        epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev); 
+                        char *add;
+                        char *now;
+                        add = inet_ntoa(cliaddr.sin_addr);
+                        now = get_time(); 
+                        access_log(access_fp, "%s connected at time %s", add, now);
+                    } else {
+                        int num;
+                        if ((num = read(events[n].data.fd, buf, MAXLINE)) <= 0) {
+                            NOTICE("pid is %d read error!\n", getpid());
+                            /*not tested yet*/
+                            close(events[n].data.fd);
+                        } else {
+                            //NOTICE("begin handle_request");
+                            handle_request(events[n].data.fd, buf);
+                            NOTICE("pid is %d handle_request DONE!!", getpid());
+                        }
+                    }
+                }
+                NOTICE("end while");
+            }//end while
+        }
+    }
+
+    if (0 == pid) { //child
+            while(1) { 
+                DEBUG("begin while");  
+                nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+                //printf("nfds == %d\n",nfds);
+                NOTICE("pid is %d epoll_wait nfds == %d\n", getpid(), nfds);
+                for (n = 0; n < nfds; n++) {
+                    if (events[n].data.fd == listenfd) {
+                        connfd = accept(listenfd, (struct sockaddr *)\
+                                &cliaddr, &clilen);
+                        NOTICE("pid is %d connfd is %d",getpid(), connfd);
+                        ev.events = EPOLLIN | EPOLLET;
+                        ev.data.fd = connfd;
+                        epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev); 
+                        char *add;
+                        char *now;
+                        add = inet_ntoa(cliaddr.sin_addr);
+                        now = get_time(); 
+                        access_log(access_fp, "%s connected at time %s", add, now);
+                    } else {
+                        int num;
+                        if ((num = read(events[n].data.fd, buf, MAXLINE)) <= 0) {
+                            NOTICE("pid is %d read error!", getpid());
+                            /*not tested yet*/
+                            close(events[n].data.fd);
+                        } else {
+                            //NOTICE("begin handle_request");
+                            handle_request(events[n].data.fd, buf);
+                            NOTICE("pid is %d handle_request DONE!!", getpid());
+                        }
+                    }
+                }
+                NOTICE("end while");
+            } 
+    
+    }
+}
